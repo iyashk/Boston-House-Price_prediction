14b54ac4352084fb07e885ea9177f678418708b8
yashwanth kiran <yashwanthkiran2624@gmail.com>
2022-07-23 23:51:43 +0530

    commit

--- gitAdd.py
+++ gitAdd.py
@@ -1,9 +1,10 @@
 import os
 from random import randint
 
-d = str(1) + 'days ago'
-with open('file. txt', 'a') as file:
-    file.write(d)
-os.system('git add .')
-os.system('git commit --date=-"' + d+'"-m"commit"')
-os. system('git push -u origin main')
+for j in range(0, randint(1, 10)):
+    d = str(j) + 'days ago'
+    # with open('file. txt', 'a') as file:
+    #     file.write(d)
+    # os.system('git add .')
+    print('git commit --date=-"' + d+'"-m"commit"')
+# os. system('git push -u origin main')
